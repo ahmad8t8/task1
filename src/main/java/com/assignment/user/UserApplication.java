6b9c5244b8d319600a682468466c267fdf50eb2c
Ahmad Ch <ahmad8t8@gmail.com>
2020-08-05 14:10:07 +0500

    library changes

--- src/main/java/com/assignment/user/UserApplication.java
+++ src/main/java/com/assignment/user/UserApplication.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class UserApplication {
 
     public static void main(String[] args) {
